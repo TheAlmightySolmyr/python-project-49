8ae6714a7c7df72e1ac16b62f3e8c921c5232642
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-02-27 00:43:29 +0300

    added prompt

--- brain_games/__init__.py
+++ brain_games/__init__.py
@@ -1,2 +1,6 @@
+import prompt
 def greet():
     print('Welcome to the Brain Games!')
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,6 @@
-from brain_games import greet
+from brain_games import greet, welcome_user
 def main():
     greet()
+welcome_user()
 if __name__ == '__main__':
     greet()
