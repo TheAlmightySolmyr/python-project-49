ba9201caaf2298b3a14c06b586eaf2a0880c050b
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-02-27 20:11:44 +0300

    first attempt doing coding

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,16 +1,37 @@
 import random, prompt
 
 def is_even(num:int):
-    if num % == 0:
+    
+    if num % 2 == 0:
         return True
     else:
         return False
 
 def even_game():
-    result = randint(1,99)
+    print(f'Answer "yes" if the number is even, otherwise answer "no".')
+    question = random.randint(1,99)
+    print(f'Question:{question}')
     answer = input()
-    if is_even(result) == True and answer.lower() == 'yes':
-        print(f'Correct!')
-    else:
-        print(f'''{answer} is wrong answer
-        Let's try again!''')
+    result = None
+    if is_even(question) == True and answer.lower() == 'yes':
+        result = True
+    elif is_even(question) == True and answer.lower() == 'no':
+        result = False
+    elif is_even(question) == False and answer.lower() == 'no':
+        result = True
+    elif is_even(question) == False and answer.lower() == 'yes':
+        result = False
+    match result:
+        case True:
+            print('Correct!')
+        case False:
+            print(f'{answer} is wrong answer. Try again!')
+
+def the_game():
+    for _ in range(4):
+        result = even_game()
+        if result == False:
+            break
+    return 'Congratulations!'
+
+the_game()
