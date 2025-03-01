4337ed142370a40e7cc9059875c975fdc040ac46
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-03-02 00:03:00 +0300

    upd cli.py, renamed brain_games.py into brain_main.py

--- brain_games/scripts/brain_main.py
+++ brain_games/scripts/brain_main.py
@@ -1,10 +1,8 @@
-from brain_games import greet, welcome_user
-
+from brain_games.cli import greet, welcome_user
 
 def main():
     greet()
     welcome_user()
-
 
 if __name__ == '__main__':
     main()
