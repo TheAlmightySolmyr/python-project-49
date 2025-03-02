3419906daa07b17e4a95e0a3fa8ed4ecc7153545
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-03-02 18:45:46 +0300

    upd brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,5 +1,5 @@
-import random
-import brain_main
+import random, brain_main, prompt
+from brain_games.cli import name
 
 
 def is_even(num: int):
@@ -9,35 +9,32 @@ def is_even(num: int):
     else:
         return False
 
-
 def even_game():
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    question = random.randint(1, 99)
-    print(f'Question:{question}')
-    answer = input()
-    result = True
-    if is_even(question) == True and answer.lower() == 'yes':
-        result = True
-    elif answer.lower() != 'yes' or 'no':
-        result = False
-    elif is_even(question) == True and answer.lower() == 'no':
-        result = False
-    elif is_even(question) == False and answer.lower() == 'no':
-        result = True
-    elif is_even(question) == False and answer.lower() == 'yes':
-        result = False
+    question_num = random.randint(1, 99)
+    print(f'Question:{question_num}')
+    answer = prompt.string('Your answer:')
+    result = 0
+    if is_even(question_num) == True and answer.lower() == 'yes':
+        result = 1
+    elif is_even(question_num) == True and answer.lower() == 'no':
+        result = 0
+    elif is_even(question_num) == False and answer.lower() == 'no':
+        result = 1
+    elif is_even(question_num) == False and answer.lower() == 'yes':
+        result = 0
     return result
 
 
 def the_game():
     for x in range(3):
-        if even_game() == True:
-            print('Correct!')
+        if even_game() == 1:
+            print(f'Correct!')
         else:
-            print('This is the wrong answer. Try again!')
+            print(f'This is the wrong answer. Try again, {name}!')
             break
     if x == 2:
-        print(f'Congratulations') 
+        print(f'Congratulations, {name}!') 
 
 brain_main.main()
 the_game()
