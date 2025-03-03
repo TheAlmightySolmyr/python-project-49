3839e55e6e4e119d245ef37dae08f2facf29f200
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-03-04 00:57:36 +0300

    added prime_mod and brain_prime

---
+++ brain_games/prime_mod.py
@@ -0,0 +1,45 @@
+import prompt
+import random
+from brain_games.cli import name
+
+
+def is_prime(num):
+    result = 0
+    for i in range(1,num+1):
+        if num % i == 0:
+            result = result + 1
+    if result == 2:
+        return True
+    else:
+        return False
+
+def prime_test():
+    num = random.randint(1,4000)
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    print(f'Question: {num}')
+    answer = prompt.string('Your answer:')
+    checked_num = is_prime(num)
+    result = 0
+    if checked_num == True and answer.lower() == 'yes' or checked_num == False and answer.lower() == 'no':
+        result = 1
+    elif checked_num == True and answer.lower() == 'no' or checked_num == False and answer.lower() == 'yes':
+        result = 0
+        if answer.lower() == 'yes':
+            print(f"{answer} is wrong answer ;(. Correct answer was 'no'")
+        elif answer.lower() == 'no':
+            print(f"{answer} is wrong answer ;(. Correct answer was 'yes'")
+    return result
+
+
+def prime_game():
+    result = 0
+    for _ in range(3):
+        if prime_test() == 1:
+            print('Correct!')
+            result = result + 1
+        else:
+            print(f'Let\'s try again, {name}!')
+            break
+    if result == 3:
+        print(f'Congratulations, {name}!')
+        
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,7 @@
+from brain_games.prime_mod import prime_game
+from brain_games.scripts.brain_main import main
+
+if __name__ == '__main__':
+    main()
+
+prime_game()
